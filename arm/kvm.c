31d1df7b47182674d70fc4b02dd558534fe0caf6
Alexandru Elisei <alexandru.elisei@arm.com>
2021-10-08 14:03:55 +0100

    arm64: Lock realm RAM in memory
    RMM doesn't yet support paging protected memory pages. Thus the VMM
    must pin the entire VM memory.
    
    Use mlock2 to keep the realm pages pinned in memory once they are faulted
    in. Use the MLOCK_ONFAULT flag to prevent pre-mapping the pages and
    maintain some semblance of on demand-paging for a realm VM.
    
    Signed-off-by: Alexandru Elisei <alexandru.elisei@arm.com>
    Signed-off-by: Suzuki K Poulose <suzuki.poulose@arm.com>

--- arm/kvm.c
+++ arm/kvm.c
@@ -6,6 +6,9 @@
 #include "kvm/fdt.h"
 
 #include "arm-common/gic.h"
+#include "asm/realm.h"
+
+#include <sys/resource.h>
 
 #include <linux/kernel.h>
 #include <linux/kvm.h>
@@ -22,6 +25,25 @@ bool kvm__arch_cpu_supports_vm(void)
 {
 	/* The KVM capability check is enough. */
 	return true;
+}
+
+static void try_increase_mlock_limit(struct kvm *kvm)
+{
+	u64 size = kvm->ram_size;
+	struct rlimit mlock_limit, new_limit;
+
+	if (getrlimit(RLIMIT_MEMLOCK, &mlock_limit)) {
+		perror("getrlimit(RLIMIT_MEMLOCK)");
+		return;
+	}
+
+	if (mlock_limit.rlim_cur > size)
+		return;
+
+	new_limit.rlim_cur = size;
+	new_limit.rlim_max = max((rlim_t)size, mlock_limit.rlim_max);
+	/* Requires CAP_SYS_RESOURCE capability. */
+	setrlimit(RLIMIT_MEMLOCK, &new_limit);
 }
 
 void kvm__init_ram(struct kvm *kvm)
@@ -44,7 +66,27 @@ void kvm__init_ram(struct kvm *kvm)
 		die("Failed to map %lld bytes for guest memory (%d)",
 		    kvm->ram_size, errno);
 
-	madvise(kvm->ram_start, kvm->ram_size, MADV_MERGEABLE);
+	/*
+	 * Do not merge pages if this is a Realm.
+	 *  a) We cannot replace a page in realm stage2 without export/import
+	 *
+	 * Pin the realm memory until we have export/import, due to the same
+	 * reason as above.
+	 *
+	 * Use mlock2(,,MLOCK_ONFAULT) to allow faulting in pages and thus
+	 * allowing to lazily populate the PAR.
+	 */
+	if (kvm__is_realm(kvm)) {
+		int ret;
+
+		try_increase_mlock_limit(kvm);
+		ret = mlock2(kvm->ram_start, kvm->ram_size, MLOCK_ONFAULT);
+		if (ret)
+			die_perror("mlock2");
+	} else {
+		madvise(kvm->ram_start, kvm->ram_size, MADV_MERGEABLE);
+	}
+
 	madvise(kvm->ram_start, kvm->ram_size, MADV_HUGEPAGE);
 
 	phys_start	= kvm->cfg.ram_addr;
